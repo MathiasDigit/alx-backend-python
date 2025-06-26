3d1f5a901fdc7eee3bed13ba2276cbd6ed60d1fa
MathiasDigit <bellmathias33@gmail.com>
2025-06-26 06:22:35 +0100

    modification of views.py and urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 from .views import UserViewSet, ConvesationViewSet, MesssageViewSet
 
-router = DefaultRouter()
+router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'conversations', ConvesationViewSet)
 router.register(r'messages', MesssageViewSet)
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,21 +1,16 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from django_filters import rest_framework as filters
 from .models import User, Conversation, Message
 from .serializers import UserSerializer, ConversationSerializer, MessageSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
-class UserViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
 class MesssageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-
+    filter_backends = [filters.DjangoFilterBackend]
+    
 class ConvesationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    
