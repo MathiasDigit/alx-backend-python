674e4a7fbf4be6351ed5520dfa3288b6f427ab5f
MathiasDigit <bellmathias33@gmail.com>
2025-07-08 11:12:43 +0100

    update2 permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,4 +1,5 @@
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 class IsOwner(BasePermission):
     """The user can only access his own messages/conversations."""
     def has_object_permission(self, request, view, obj):
