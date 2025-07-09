0a394958b3d02b8f3e6271b7b2fb159dca6c76b7
MathiasDigit <bellmathias33@gmail.com>
2025-07-09 14:11:15 +0100

    update views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, status
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 from rest_framework.permissions import IsAuthenticated
-from .permissions import IsOwner
+from .permissions import IsParticipantOfConversation
 from .filters import MessageFilter
 from .pagination import CustomPagination
 from django_filters.rest_framework import DjangoFilterBackend
@@ -12,7 +12,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_class = [IsAuthenticated, IsOwner] # Ensure only owners can access messages
+    permission_class = [IsAuthenticated, IsParticipantOfConversation] # Ensure only owners can access messages
     pagination_class = CustomPagination # Enable pagination for messages
     filter_backends = [DjangoFilterBackend]  # Enable django-filter backend
     filterset_class = MessageFilter  # Apply custom filter clas
@@ -25,8 +25,8 @@ class MessageViewSet(viewsets.ModelViewSet):
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated, IsOwner]
-    
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+
 
 
 
