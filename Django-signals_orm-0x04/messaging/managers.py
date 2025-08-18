cc4acf063c56e3d32620662595fdb4c2785ed4cd
MathiasDigit <bellmathias33@gmail.com>
2025-08-18 17:12:43 +0100

    update manages.py

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,12 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 
-@login_required
-def unread_inbox(request):
-    user = request.user
-
-    # Récupère les messages non lus uniquement, avec optimisation .only()
-    unread_messages = Message.unread.for_user(user).select_related('sender')
-
-    return render(request, 'inbox_unread.html', {'messages': unread_messages})
+class UnreadMessagesManager(models.Manager):
+    def for_user(self, user):
+        return self.filter(receiver=user, read=False).only('id', 'sender', 'content', 'timestamp')
